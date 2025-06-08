feda5f64d9ad69fd0487b00c61f2ca5347ac94bb
joshdsauter <josh.d.sauter@gmail.com>
2025-06-08 12:12:24 -0700

    Remove unused import directives in NoteItem.

--- app/src/main/java/com/example/simplenotesapp/ui/components/NoteItem.kt
+++ app/src/main/java/com/example/simplenotesapp/ui/components/NoteItem.kt
@@ -11,14 +11,12 @@ import androidx.compose.foundation.layout.wrapContentHeight
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Delete
 import androidx.compose.material.icons.filled.Edit
-import androidx.compose.material3.AlertDialog
 import androidx.compose.material3.Card
 import androidx.compose.material3.CardDefaults
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextButton
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.getValue
